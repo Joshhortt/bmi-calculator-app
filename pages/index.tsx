e12f2115e59b0a1bbbc7e33d346007ccf282b628
Joshhortt <joshhortt@yahoo.com>
2022-06-14 21:59:47 +0100

    add inputs and calculate button to get Bmi

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,22 @@ export default function Home() {
 			<h3 className="text-secondary text-2xl font-raleway font-bold uppercase tracking-wide mb-14 md:text-base md:px-4 md:text-center">
 				Calculate your BMI using weight and height
 			</h3>
+      <div className="flex flex-col justify-between items-center w-full md:items-center">
+				<form className="flex w-full justify-center md:flex-col md:w-5/6">
+					<input
+						autoFocus={true}
+						className="border-none outline-none bg-primary px-4 py-2 w-1/6 mx-2 rounded-sm font-raleway md:w-full md:mx-0 md:my-4"
+						placeholder="Enter your weight (in kgs)..."
+					/>
+					<input
+						className="border-none outline-none bg-primary px-4 py-2 w-1/6 mx-2 rounded-sm font-raleway md:w-full md:mx-0"
+						placeholder="Enter your height (in meters)..."
+					/>
+					<button className="outline-none border border-danger font-bold font-raleway ml-4 px-12 py-2 rounded-sm bg-danger text-lightGrey transition duration-300 hover:bg-bc hover:text-primary md:ml-0 md:mt-4">
+						Calculate
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 }
